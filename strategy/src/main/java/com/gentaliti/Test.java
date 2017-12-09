b6fcffaf011749628c3bb1604dc94653307e44e8
Gent Aliti <aliti.genti@gmail.com>
2017-12-09 19:25:25 +0100

    Configured log4j, added strategy module

---
+++ strategy/src/main/java/com/gentaliti/Test.java
@@ -0,0 +1,13 @@
+package com.gentaliti;
+
+import org.apache.log4j.Logger;
+
+
+public class Test {
+
+    private static Logger logger = Logger.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("Test");
+    }
+}
---
+++ strategy/src/test/java/com/gentaliti/TestClass.java
@@ -0,0 +1,9 @@
+package com.gentaliti;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import org.junit.Test;
+
+public class TestClass {
+
+}
